bad3198c00f89dbb79b40184831f55392d36c1dd
Steffen Müller <smueller@cpan.org>
2005-01-21 14:02:45 +0000

    Import of SMUELLER/Bundle-Math-Symbolic-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-Math-Symbolic
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         SMUELLER/Bundle-Math-Symbolic-1.00.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

---
+++ Symbolic.pm
@@ -0,0 +1,50 @@
+package Bundle::Math::Symbolic;
+use strict;
+use warnings;
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Math::Random - Bundle of modules related to symbolic math
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::Math::Symbolic'
+
+=head1 CONTENTS
+
+Math::Algebra::Symbols
+
+Math::Symbolic
+
+Math::Symbolic::Custom::CCompiler
+
+Math::Symbolic::Custom::ErrorPropagation
+
+Math::SymbolicX::BigNum
+
+Math::SymbolicX::Complex
+
+Math::SymbolicX::NoSimplification
+
+Math::SymbolicX::ParserExtensionFactory
+
+=head1 DESCRIPTION
+
+This is a bundle of modules related to symbolic math.
+
+Please have a look at L<Bundle::Math>.
+
+If you would like to see a specific module included in a future version of
+this bundle, please send me an email or use rt.cpan.org.
+
+=head1 AUTHOR
+
+Steffen Mueller E<lt>bundlemath-module at steffen-mueller dot netE<gt>
+
+=cut 
